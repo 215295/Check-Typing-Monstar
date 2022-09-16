f0183b46001481fff491d85c60f711b33fb5353e
215295 <sumonasad04717@gmail.com>
2022-09-17 00:42:36 +0600

    added

--- script.js
+++ script.js
@@ -108,19 +108,30 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-
+   
     // finished timer
-    if (count == 0) {
+    if (count == -1) {
+      
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
+      
       display.classList.remove("inactive");
+      
 
       clearInterval(startCountdown);
+      
       startTime = new Date().getTime();
+      
+      
     }
+   
     count--;
+    
   }, 1000);
+
+  
+ 
 };
 
 // START Countdown
@@ -134,6 +145,6 @@ setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
 
-
+  
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
